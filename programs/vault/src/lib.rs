3241b26d2ebb5205167bb46ebe12ec802dcaf544
Abhi16016 <abhinavkale919913@gmail.com>
2025-07-11 16:44:31 +0530

    Vault challenge from BlueShift

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -1,21 +1,67 @@
-pub mod constants;
-pub mod error;
-pub mod instructions;
-pub mod state;
-
 use anchor_lang::prelude::*;
-
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
+use anchor_lang::system_program::{transfer, Transfer};
 
 declare_id!("BZxRHnWJV4c9Sncp7GeK9PcwhNRYRuL5EtxaJWfzzSf6");
-
+ 
 #[program]
 pub mod vault {
+    use anchor_lang::system_program::Transfer;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        require_eq!(ctx.accounts.vault.lamports(), 0, VaultError::VaultAlreadyExists);
+        require_eq!(amount, Rent::get()?.minimum_balance(0), VaultError::InvalidAmount);
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.signer.to_account_info(),
+                    to: ctx.accounts.vault.to_account_info(),
+                }
+            ), amount)?;
+        Ok(())
     }
+
+    pub fn withdraw(ctx: Context<VaultAction>) -> Result<()> {
+        require_eq!(ctx.accounts.vault.lamports(), 0, VaultError::InvalidAmount);
+
+        let signer_key = ctx.accounts.signer.key();
+        let signer_seeds = &[b"vault", signer_key.as_ref(), &[ctx.bumps.vault]];
+        
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.vault.to_account_info(),
+                    to: ctx.accounts.signer.to_account_info() 
+                },
+                &[&signer_seeds[..]]
+            ),
+            ctx.accounts.vault.lamports()
+        )?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct VaultAction<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", signer.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum VaultError {
+    #[msg("Vault Already exists")]
+    VaultAlreadyExists,
+    #[msg("Invalid Amount")]
+    InvalidAmount,
 }
